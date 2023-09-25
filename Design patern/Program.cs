f092c537bb621220b24598a12c24de301f83096b
Omar Soliman <omar.soliman.elsedawy99@gmail.com>
2023-09-25 22:36:50 +0300

    Init Commit

---
+++ Design patern/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+namespace FactoryDesignPattern
+{
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+    class MoneyBack : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+    public class Titanium : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+    public class Platinum : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+    public class CreditCardFactory
+    {
+        public static CreditCard GetCreditCard(string cardType)
+        {
+            CreditCard cardDetails = null;
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            return cardDetails;
+        }
+    }
+    //without factory pattern
+   /* class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generally we will get the Card Type from UI.
+            //Here we are hardcoded the card type
+            string cardType = "MoneyBack";
+            CreditCard cardDetails = null;
+            //Based of the CreditCard Type we are creating the
+            //appropriate type instance using if else condition
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard cardDetails = CreditCardFactory.GetCreditCard(Console.ReadLine());
+
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }
+}
